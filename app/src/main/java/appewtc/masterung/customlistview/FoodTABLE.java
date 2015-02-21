80172bad5d99f0f7bd1a046425ed9be0321f9ba2
masterUNG <phrombutr@gmail.com>
2015-02-22 02:07:48 +0700

    ListFood OK

--- app/src/main/java/appewtc/masterung/customlistview/FoodTABLE.java
+++ app/src/main/java/appewtc/masterung/customlistview/FoodTABLE.java
@@ -1,6 +1,8 @@
 package appewtc.masterung.customlistview;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 /**
@@ -11,6 +13,11 @@ public class FoodTABLE {
     private MyOpenHelper objMyOpenHelper;
     private SQLiteDatabase writeSQLite, readSQLite;
 
+    public static final String TABEL_FOOD = "foodTABLE";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_FOOD = "Food";
+    public static final String COLUMN_PRIVE = "Price";
+
     public FoodTABLE(Context context) {
 
         objMyOpenHelper = new MyOpenHelper(context);
@@ -18,5 +25,33 @@ public class FoodTABLE {
         readSQLite = objMyOpenHelper.getReadableDatabase();
 
     }   // Constructor
+
+    public String[] listFood() {
+
+        String strListFood[] = null;
+
+        Cursor objCursor = readSQLite.query(TABEL_FOOD, new String[]{COLUMN_ID, COLUMN_FOOD}, null, null, null, null, null);
+
+        strListFood = new String[objCursor.getCount()];
+
+        objCursor.moveToFirst();
+
+        for (int i = 0; i < objCursor.getCount(); i++) {
+
+            strListFood[i] = objCursor.getString(objCursor.getColumnIndex(COLUMN_FOOD));
+            objCursor.moveToNext();
+
+        }   //for
+        objCursor.close();
+
+        return strListFood;
+    }
+
+    public long addValueFood(String strFood, String strPrice) {
+        ContentValues objConteValue = new ContentValues();
+        objConteValue.put(COLUMN_FOOD, strFood);
+        objConteValue.put(COLUMN_PRIVE, strPrice);
+        return writeSQLite.insert(TABEL_FOOD, null, objConteValue);
+    }
 
 }   // Main Class
--- app/src/main/java/appewtc/masterung/customlistview/MainActivity.java
+++ app/src/main/java/appewtc/masterung/customlistview/MainActivity.java
@@ -1,10 +1,26 @@
 package appewtc.masterung.customlistview;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -19,11 +35,22 @@ public class MainActivity extends ActionBarActivity {
         //Create Or Connected Database
         createOrConnectedDatabase();
 
+        //Tester
+        //objFoodTABLE.addValueFood("Food", "Price");
+
+        //Delete All Data
+        deleteAllData();
+
+        //Synchronize
+        synJSONtoSQLite();
+
         int[] intTarget = {R.drawable.food1, R.drawable.food2, R.drawable.food3, R.drawable.food4, R.drawable.food5, R.drawable.food6, R.drawable.food7, R.drawable.food8, R.drawable.food9, R.drawable.food10,
                 R.drawable.food11, R.drawable.food12, R.drawable.food13, R.drawable.food14, R.drawable.food15,
                 R.drawable.food16, R.drawable.food17, R.drawable.food18, R.drawable.food19, R.drawable.food20};
 
-        String[] strNameFood = getResources().getStringArray(R.array.food);
+        //String[] strNameFood = getResources().getStringArray(R.array.food);
+
+        String[] strNameFood = objFoodTABLE.listFood();
 
         //Create Adapter
         myAdapter objmyAdapter = new myAdapter(getApplicationContext(), strNameFood, intTarget);
@@ -33,6 +60,67 @@ public class MainActivity extends ActionBarActivity {
 
 
     }   // onCreate
+
+    private void deleteAllData() {
+        SQLiteDatabase objSQLite = openOrCreateDatabase("my_menu.db", MODE_PRIVATE, null);
+        objSQLite.delete("foodTABLE", null, null);
+    }   // deleteAllData
+
+    private void synJSONtoSQLite() {
+
+        if (Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(myPolicy);
+        }   // if
+
+
+        InputStream objInputStream = null;
+        String strJSON = "";
+
+        try {
+
+            HttpClient objHttpClient = new DefaultHttpClient();
+            HttpPost objHttpPost = new HttpPost("http://swiftcodingthai.com/rest/get_data_food_rest.php");
+            HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+            HttpEntity objHttpEntity = objHttpResponse.getEntity();
+            objInputStream = objHttpEntity.getContent();
+
+        } catch (Exception e) {
+            Log.d("memu", "InputStream ==> " + e.toString());
+        }
+
+
+        try {
+
+            BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader(objInputStream, "UTF-8"));
+            StringBuilder objStringBuilder = new StringBuilder();
+            String strLine = null;
+            while ((strLine = objBufferedReader.readLine()) != null) {
+                objStringBuilder.append(strLine);
+            }
+            objInputStream.close();
+            strJSON = objStringBuilder.toString();
+        } catch (Exception e) {
+            Log.d("memu", "strJSON ==> " + e.toString());
+        }
+
+
+        try {
+
+            final JSONArray objJSONArray = new JSONArray(strJSON);
+            for (int i = 0; i < objJSONArray.length(); i++) {
+                JSONObject objJSONObjcet = objJSONArray.getJSONObject(i);
+                String strFood = objJSONObjcet.getString("Food");
+                String strPrice = objJSONObjcet.getString("Price");
+                objFoodTABLE.addValueFood(strFood, strPrice);
+            }   // for
+
+        } catch (Exception e) {
+            Log.d("memu", "update SQLite ==> " + e.toString());
+        }
+
+
+    }   // synJSONtoSQLite
 
     private void createOrConnectedDatabase() {
         objFoodTABLE = new FoodTABLE(this);
